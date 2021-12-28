a690f037d9881152bc246b33c621bf6e8b2d33ea
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2021-12-28 16:22:13 -0300

    fix: registrar eventos

--- src/main/java/com/saed/agenda/controller/EventoController.java
+++ src/main/java/com/saed/agenda/controller/EventoController.java
@@ -9,53 +9,52 @@ import com.saed.agenda.ejb.EventosFacadeLocal;
 import com.saed.agenda.entities.Eventos;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
 
 /**
  *
  * @author pc
  */
 @Named(value = "eventoController")
-@javax.faces.view.ViewScoped
+@RequestScoped
+public class EventoController {
 
-public class EventoController implements java.io.Serializable {
+  @EJB
+  private EventosFacadeLocal eventoEJB;
+  private Eventos evento = new Eventos();
 
-    @EJB
-    private EventosFacadeLocal eventoEJB;
-    private Eventos evento = new Eventos();
+  public Eventos getEvento() {
+    return evento;
+  }
 
-    public Eventos getEvento() {
-        return evento;
+  public void setEvento(Eventos evento) {
+    this.evento = evento;
+  }
+
+  public void addEvent() {
+    System.out.println("Se invoca");
+    try {
+      eventoEJB.create(evento);
+      //evento = new Evento();
+      //aqui debo poner un println para que avise que el evento se agrego corretamente
+    } catch (Exception e) {
+      System.out.println("Ocurrio un error: " + e);
     }
 
-    public void setEvento(Eventos evento) {
-        this.evento = evento;
+  }
+
+  public String remove() {
+
+    try {
+      eventoEJB.create(evento);
+      evento = new Eventos();
+      //aqui debo poner un println para que avise que el evento se elimino corretamente
+    } catch (Exception e) {
+
+      //aqui debo poner un println para que avise que el evento no se pudo elimnar
     }
 
-    public String create() {
-        try {
-            eventoEJB.create(evento);
-            evento = new Eventos();
-            //aqui debo poner un println para que avise que el evento se agrego corretamente
-        } catch (Exception e) {
-        //aqui debo poner un println para que avise que el evento no se pudo crear
-        }
-        
-        return null;
-    }
+    return null;
+  }
 
-    public String remove() {
-
-        try {
-            eventoEJB.create(evento);
-            evento = new Eventos();
-             //aqui debo poner un println para que avise que el evento se elimino corretamente
-        } catch (Exception e) {
-            
-            //aqui debo poner un println para que avise que el evento no se pudo elimnar
-        }
-
-        return null; 
-    }
-    
 }
